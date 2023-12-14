c7f2a13336e4e86f938ea1dbc2d556f37bd7f655
unknown <christianhonore@outlook.com>
2023-12-14 10:18:08 +0100

    ok

---
+++ nombre.d.enfant.php
@@ -0,0 +1,9 @@
+*/ Ecrivez un programme PHP qui dit "Hey, salut à tous les amis, c'est moi le
+programme PHP !"
+Demandez à l'utilisateur combien d'enfant il a. Si il n'en a pas, dites lui
+dommage, si il en a moins que 3, dites lui qu'il a une petite famille, si il en a plus
+que 3, dites lui qu'il a une famille nombreuse.*/
+<?php
+echo  "Hey, salut à tous les amis, c'est moi le
+programme PHP !";
+
