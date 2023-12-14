0770ef9dc966efd83f9c1a039c9ce47d8cc4c7c2
unknown <christianhonore@outlook.com>
2023-12-14 10:33:57 +0100

    exercice

--- nombre.d.enfant.php
+++ nombre.d.enfant.php
@@ -6,4 +6,14 @@ que 3, dites lui qu'il a une famille nombreuse.*/
 <?php
 echo  "Hey, salut à tous les amis, c'est moi le
 programme PHP !";
-
+echo "\n combien d enfant avez vous ? " ;
+$enfant=trim(fgets( STDIN)) ;
+if ( $enfant==0) {
+    echo " dommage " ;
+}
+if ($enfant>0 && $enfant<=3){
+    echo " vous avez une petite famille " ;
+}
+elseif ($enfant>3) {
+    echo " vous etes une famille nombreuse";
+}
