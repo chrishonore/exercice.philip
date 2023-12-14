3a0db80667a87bdbfdacccf8f3511d152aef47d0
unknown <christianhonore@outlook.com>
2023-12-14 11:33:59 +0100

    morpion

---
+++ jeux.a.2.php
@@ -0,0 +1,12 @@
+<?php
+/*Implémentez le jeu du Morpion (Tic Tac Toe). Créez une grille 3x3 et permettez
+à deux joueurs de jouer tour à tour. Demandez à chaque joueur de saisir les
+Algorithmie
+Pratique
+coordonnées où il souhaite placer son symbole (X ou O). Utilisez des boucles
+imbriquées pour afficher la grille et vérifier si un joueur a gagné après chaque
+coup. Le jeu doit se poursuivre jusqu'à ce qu'il y ait un vainqueur ou un match
+nul.
+*/
+$joueur1="toto";
+$joueur2="lulu";
